6950a0cb5bf441f99ffd04c75f2d89ec08861d74
Sarthak Parkale <143308922+Failureguy94@users.noreply.github.com>
2025-06-07 13:52:50 +0530

    Update Bit_Strings.cpp

--- CSES/Introductory_Problems/Bit_Strings.cpp
+++ CSES/Introductory_Problems/Bit_Strings.cpp
@@ -1,12 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+const int MOD=1e9+7;
+int pow(int a,int b){
+    int ans=1;
+    // int a=2;
+    while(b){
+      if(1&b){
+        ans=(ans*1LL*a)%MOD;
+      }
+      a=(a*a*1LL)%MOD;
+      b=b>>1;
+    }
+    return ans;
+}
+
 void solve() {
     int n;
     cin>>n;
-    int MOD=1e9+7;
-    int k=(1LL<<n/2);
-    cout << (2*(1<<(n/2)))%MOD<<endl;
+    // int MOD=1e9+7;
+    int a=2;
+    cout << (pow(a,n))%MOD<<endl;
 }
 
 int32_t main() {
