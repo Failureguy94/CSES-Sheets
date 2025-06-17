1efdccdd23b29dd2e506e57482dbbef52c4218f3
Sarthak Parkale <143308922+Failureguy94@users.noreply.github.com>
2025-06-17 14:36:04 +0530

    Create Ferris_Wheel.cpp

---
+++ CSES/Sorting and Searching/Ferris_Wheel.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+vector<int> unique(vector<int> v) {
+    sort(v.begin(), v.end());
+    auto it = unique(v.begin(), v.end());
+    v.erase(it, v.end());
+return v;
+}
+#define as(v) sort(v.begin(), v.end())
+#define coutyes cout<<"YES"<<endl
+#define coutno cout<<"NO"<<endl
+#define as(v) sort(v.begin(), v.end())
+#define ds(v) sort(v.begin(), v.end(), greater<>())
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define ceil(a, b) (a + b - 1) / b
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+void solve() {
+    int n,x;
+    cin>> n>>x;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>> v[i];
+
+    }
+    as(v);
+    int i=0;
+    int j=n-1;
+    int ans=0;
+    while(i<=j){
+        if(v[i]+v[j]<=x){
+            ans++;
+            i++;
+            j--;
+        }
+        else{
+            ans++;
+            j--;
+        }
+    }
+    cout << ans<<endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+        solve();
+    
+
+    return 0;
+}
