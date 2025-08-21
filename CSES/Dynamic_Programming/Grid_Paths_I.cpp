df435db53a00e3bac4b3874a76de1888adb61336
Sarthak Parkale <143308922+Failureguy94@users.noreply.github.com>
2025-08-21 17:58:37 +0530

    Add files via upload

---
+++ CSES/Dynamic_Programming/Grid_Paths_I.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MOD =1e9+7;
+vector<int> unique(vector<int> v) {
+    sort(v.begin(), v.end());
+    auto it = unique(v.begin(), v.end());
+    v.erase(it, v.end());
+return v;
+}
+    int toint(char c){ return c-'0'; }
+#define coutyes cout<<"YES"<<endl
+#define coutno cout<<"NO"<<endl
+#define as(v) sort(v.begin(), v.end())
+#define sum(v) accumulate(v.begin(), v.end(),0LL)
+#define ds(v) sort(v.begin(), v.end(), greater<>())
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define ceil(a, b) (a + b - 1) / b
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+
+// Template :https://github.com/Failureguy94/Template-for-Competitive-Programming
+int recur(int r,int c,int m,int n,vector<vector<int>> &dp){
+    if(r>=m || c>=n || dp[r][c]==-1){
+        return 0;
+    }
+    if(r==m-1 && c==n-1){
+        return 1;
+    }
+    if(dp[r][c]!=0){
+       return dp[r][c];
+    }
+    return dp[r][c]=recur(r+1,c,m,n,dp)+recur(r,c+1,m,n,dp);
+}
+void solve() {
+    // Template :https://github.com/Failureguy94/Template-for-Competitive-Programming
+        int n;
+        cin >> n;
+        vector<vector<char>> v(n,vector<char>(n));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+            cin>>v[i][j];
+            }
+        }
+vector<vector<int>>dp(n,vector<int> (n,0));
+if(v[0][0]=='.'){
+ dp[0][0]=1;    
+}
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        if(v[i][j]!='.'){
+             continue;
+        }
+        else{
+           if(i>0){
+            dp[i][j]+=dp[i-1][j];
+           }
+           if(j>0){
+            dp[i][j]+=dp[i][j-1];
+           }
+        }
+        dp[i][j]=dp[i][j]%MOD;
+    }
+}
+cout << dp[n-1][n-1]<<endl;
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+solve();
+
+    return 0;
+}
