dd2b65b59263aafe9ef19b8e29e1e8d4fda155f7
Failureguy94 <sarthak14313@gmail.com>
2025-12-26 16:35:28 +0530

    solved using Segment tree

--- CSES/Range Queries/Static_Range_Minimum_Queries.cpp
+++ CSES/Range Queries/Static_Range_Minimum_Queries.cpp
@@ -10,6 +10,8 @@ return v;
 }
     int toint(char c){ return c-'0'; }
 #define coutyes cout<<"YES"<<endl
+int addm(int a,int b){ return (a%MOD + b%MOD)%MOD; }
+int mulm(int a,int b){ return ((a%MOD)*(b%MOD))%MOD; }
 #define coutno cout<<"NO"<<endl
 #define as(v) sort(v.begin(), v.end())
 #define sum(v) accumulate(v.begin(), v.end(),0LL)
@@ -23,9 +25,63 @@ return v;
 #define ceil(a, b) (a + b - 1) / b
 string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
 string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
- 
+
 // Template :https://github.com/Failureguy94/Template-for-Competitive-Programming
- 
+struct MinSegmentTree {
+    struct Node {
+        int l, r, val;
+        Node* left;
+        Node* right;
+
+        Node(int _l, int _r)
+            : l(_l), r(_r), val(INT_MAX), left(nullptr), right(nullptr) {}
+
+        int mid() { return (l + r) >> 1; }
+    };
+
+    Node* root;
+    vector<int>& arr;
+
+    MinSegmentTree(vector<int>& a) : arr(a) {
+        root = new Node(0, (int)a.size() - 1);
+        build(root);
+    }
+
+    void build(Node* node) {
+        if (node->l == node->r) {
+            node->val = arr[node->l];
+            return;
+        }
+
+        int m = node->mid();
+        node->left = new Node(node->l, m);
+        node->right = new Node(m + 1, node->r);
+
+        build(node->left);
+        build(node->right);
+
+        node->val = min(node->left->val, node->right->val);
+    }
+
+    int query(Node* node, int l, int r) {
+        if (!node || r < node->l || l > node->r)
+            return INT_MAX;
+
+        if (l <= node->l && node->r <= r)
+            return node->val;
+
+        return min(
+            query(node->left,l,r),
+            query(node->right,l,r)
+        );
+    }
+
+    int get(int l, int r) {
+        if (l>r) return INT_MAX;
+        return query(root, l, r);
+    }
+};
+
 void solve() {
     // Template :https://github.com/Failureguy94/Template-for-Competitive-Programming
         int n,q;
@@ -33,22 +89,19 @@ void solve() {
         vector<int> v(n);
         for(int i=0;i<n;i++){
             cin>>v[i];
-        }
-        vector<int> presum(n+1);
-        int sum = 0;
-        for (int i = 0; i <n; i++) {
-            sum += v[i];
-            presum[i+1] = sum;
-        }
-        while(q--){
-            int a,b;
-            cin>>a>>b;
-            cout << presum[b]-presum[a-1]<<endl;
-        }
+    }
+    MinSegmentTree S(v);
+    while(q--){
+        int a,b;
+        cin>>a>>b;
+        cout << S.get(a-1,b-1)<<endl;
+    }
 }
+
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
         solve();
+
     return 0;
 }
